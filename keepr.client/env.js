52ae85b2c817b00feb717070b6fba62df7798b26
corymccormick <corym.otf@gmail.com>
2021-05-28 14:39:03 -0600

    initial setup

---
+++ keepr.client/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'corym.us.auth0.com'
+export const audience = 'https://keepr/'
+export const clientId = 'RXH4gnrquOsOUlZarjwOwsaigMLYzW6y'
